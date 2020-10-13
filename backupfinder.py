5eb2fb6bd67a07d25c49663a231c1b13befffb5f
Parrot user <user@localhost.localdomain>
2020-10-13 08:54:15 +0100

    fix: move colors'

--- backupfinder.py
+++ backupfinder.py
@@ -1,16 +1,7 @@
 #!/usr/bin/python3
 import sys
 import requests
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+from bcolors import Bcolors 
 
 if len(sys.argv) != 2:
     print('ERROR')
@@ -22,6 +13,7 @@ else:
 
 f = open("backup_extensions.txt", "r")
 backup_types_list = f.read().split('\n')
+bcolors = Bcolors
 
 for backup_type in backup_types_list:
     try:
